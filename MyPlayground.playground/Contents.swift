7d071942e8a91d33c9d28eb8b098496a96f1a477
dhimanranjit1990@gmail.com <dhimanranjit197@gmail.com>
2017-02-06 16:51:03 +0530

    initial project version

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let i = 4, j = 2, k = 4
+
+if (i | j) == k {
+    print("true")
+} else {
+    print("false")
+}
